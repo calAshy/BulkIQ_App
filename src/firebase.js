54a638091c17a15b5103c8d8d0079e58d30d3086
Brandon <22515116@stu.mmu.ac.uk>
2025-06-06 17:09:07 +0100

    Add firebase config and initialization

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJedbXUCmAkqkikt73_4XlH1jjdXEuuWY",
+  authDomain: "bulkiqapp.firebaseapp.com",
+  projectId: "bulkiqapp",
+  storageBucket: "bulkiqapp.firebasestorage.app",
+  messagingSenderId: "769478007293",
+  appId: "1:769478007293:web:b55cebc915410b1985a194",
+  measurementId: "G-31DN2KZVXB",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Auth
+const auth = getAuth(app);
+
+export { auth };
