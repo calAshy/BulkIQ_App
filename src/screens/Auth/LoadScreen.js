86846c31564227bca975ba2a9df0fd14282ff3f5
Luke Ashford <lukeashford@192.168.1.109>
2025-06-07 19:36:19 +0100

    LoadScreen starter code added.

---
+++ src/screens/Auth/LoadScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function LoadScreen() {
+    return (
+        <View>
+            <Text>Load Scren Test.</Text>
+        </View>
+    )
+}
