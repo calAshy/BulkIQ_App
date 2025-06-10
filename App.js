693f7e5441bb4b800c8b6444a19bd483bbfbe6cc
Brandon <22515116@stu.mmu.ac.uk>
2025-06-10 13:53:27 +0100

    Set up Login Screen File

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SignUpScreen from './src/screens/Auth/SignUpScreen';
-import LoadScreen from './src/screens/Auth/LoadScreen';
-import LoginScreen from './src/screens/Auth/LoginScreen';
-
-
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SignUpScreen from "./src/screens/Auth/SignUpScreen";
+import LoadScreen from "./src/screens/Auth/LoadScreen";
+import LoginScreen from "./src/screens/Auth/LoginScreen";
 
 export default function App() {
   return (
     <>
-    <SignUpScreen/>
+      <LoginScreen />
     </>
   );
 }
---
+++ src/screens/Auth/LoginScreen.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default function LoginScreen() {
+  return (
+    <LinearGradient
+      colors={["#1d1d1d", "#0a0a0a", "#0a0a0a", "#1d1d1d"]}
+      style={styles.gradientStyles}
+    >
+      <SafeAreaView style={styles.container}>
+        <View style={styles.TitleAndTagLineStyles}>
+          <Text style={styles.Wordmark}>BULK IQ</Text>
+          <Text style={styles.text}>Login In</Text>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  gradientStyles: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    width: "100%",
+  },
+  TitleAndTagLineStyles: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  Wordmark: {
+    color: "#fff",
+    fontWeight: "800",
+    fontSize: 40,
+    marginBottom: 8,
+  },
+  text: {
+    color: "#fff",
+  },
+});
