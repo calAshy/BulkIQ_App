740b95a64b572cba8bc6402dfd8aea2fab9d8da0
Luke Ashford <lukeashford@192.168.1.109>
2025-06-13 08:29:32 +0100

    Changed original firebase file name. Added new firebase.js that contains created functions for our AuthFlow.

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { auth  } from "./firebaseInit";
+import { signInWithEmailAndPassword } from "@react-native-firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCJedbXUCmAkqkikt73_4XlH1jjdXEuuWY",
-  authDomain: "bulkiqapp.firebaseapp.com",
-  projectId: "bulkiqapp",
-  storageBucket: "bulkiqapp.firebasestorage.app", // IMPORTANT!! chatgpt reckons this has an issue but i do not want to change it.
-  messagingSenderId: "769478007293",
-  appId: "1:769478007293:web:b55cebc915410b1985a194",
-  measurementId: "G-31DN2KZVXB",
-};
+signInWithEmailAndPassword(auth, email, password)
+.then(userCredential => {
+    alert('User Logged in: ', userCredential.user);
+})
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Firebase Auth
-const auth = getAuth(app);
-
-export { auth };
+.catch(error => {
+    alert('Login Error', error);
+})
